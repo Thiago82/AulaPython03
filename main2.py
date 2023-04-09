cd40132d8fcf4791c02c076f40d131452965c0d5
Thiago82 <129586490+Thiago82@users.noreply.github.com>
2023-04-09 20:29:31 -0300

    Update main2.py
    linha 11 corrigida

--- main2.py
+++ main2.py
@@ -8,7 +8,7 @@ from post import Post
 app_user_one = User("jeca@hotmail.com", "Jeca Jequiti", "1234","DevOps Engineer")
 app_user_one.get_user_info()
 
-app_user_two = User("hunu_123@hotmail.com", "Marcondes", "janeteboquete","Faxineiro")
+app_user_two = User("hunu_123@hotmail.com", "Marcondes", "janete123","Faxineiro")
 app_user_one.get_user_info()
 
 new_post = Post("Limpando o banheiro no momento", app_user_two.name)
